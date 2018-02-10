f0fd2cfe1cdca589d0a46805ee7357709991cd85
Bruce Ediger <bediger8@gmail.com>
2018-02-10 09:22:36 -0700

    Use bufio to read bytes from file, rather than file in memory.

--- ic.go
+++ ic.go
@@ -4,79 +4,68 @@
  * IC = sum(n_i*(n_i - 1)*c / (N*(N-1))
  * n_i = observed letter counts in ciphertext
  *   N = total letters in ciphertext
- *   c = letters in alphabet
+ *   c = size of alphabet
  */
 
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
-func handleFn(file *os.File) func(error) {
-	return func(err error) {
-		if err != nil {
-			file.Close()
-			log.Fatal(err)
-		}
-	}
-}
-
-func readFile(fileName string) (int, []byte) {
-
-	fin, err := os.Open(fileName)
-	handle := handleFn(fin)
-	handle(err)
-
-	fileinfo, err := fin.Stat()
-	handle(err)
-
-	filesize := fileinfo.Size()
-	buffer := make([]byte, filesize)
-
-	bytesread, err := fin.Read(buffer)
-	handle(err)
-
-	fin.Close()
-
-	return bytesread, buffer
-}
-
 func main() {
 
 	if len(os.Args) == 1 {
-		fmt.Printf("%s: need a file name on command line\n", os.Args[0])
+		fmt.Printf("%s: need options or a file name on command line\n", os.Args[0])
 		os.Exit(1)
 	}
 
-	byteCount, byteBuffer := readFile(os.Args[1])
+	fileName := os.Args[1]
 
-	fmt.Printf("Read %d bytes\n", byteCount)
-	fmt.Printf("Buffer size %d\n", len(byteBuffer))
+	fin, err := os.Open(fileName)
+	if err != nil {
+		fin.Close()
+		log.Fatal(err)
+	}
+	defer fin.Close()
+
+	rdr := bufio.NewReader(fin)
 
 	var counts [256]int
 	var byteUsed [256]bool
 
-	for i := 0; i < byteCount; i++ {
-		counts[byteBuffer[i]]++
-		byteUsed[byteBuffer[i]] = true
+	var b byte
+	var e error
+	var byteCount int
+
+	for b, e = rdr.ReadByte(); e == nil; b, e = rdr.ReadByte() {
+		byteCount++
+		counts[b]++
+		byteUsed[b] = true
+	}
+
+	if e != nil && e != io.EOF {
+		fmt.Fprintf(os.Stderr, "Problem reading byte at offset %d: %s\n", byteCount, e)
 	}
 
 	var s1 float64
 
 	for i := 0; i < 256; i++ {
-		s1 += float64(counts[i]*(counts[i] - 1))
+		s1 += float64(counts[i] * (counts[i] - 1))
 	}
 
 	var N float64
 	for _, appears := range byteUsed {
-		if appears { N++ }
+		if appears {
+			N++
+		}
 	}
 
-	ic := s1*N/float64(byteCount*(byteCount - 1))
+	ic := s1 / float64(byteCount*(byteCount-1))
 
-	fmt.Printf("Alphabet of %.0f size\n", N)
-	fmt.Printf("Index of coincidence %f\n", ic)
+	fmt.Printf("%s\t%d\t%.0f\t%.3f\n", fileName, byteCount, N, ic)
 }
